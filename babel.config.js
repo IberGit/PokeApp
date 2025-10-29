5af3bfb7e5b93f65492a5989c6959199b6c93d2c
Damian <dsimonib@gmail.com>
2025-10-29 12:25:41 +0100

    Add Alias to babel and ts configs. Install React Native Paper

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,23 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./src'], // Base para las rutas personalizadas
+        alias: {
+          '@assets': './src/assets',
+          '@components': './src/components',
+          '@costants': './src/costants',
+          '@navigation': './src/navigation',
+          '@screens': './src/screens',
+          '@services': './src/services',
+          '@styles': './src/styles',
+          '@utils': './src/utils',
+        },
+      },
+    ],
+    'react-native-paper/babel',
+    'react-native-reanimated/plugin', // Este debe ir al final segun la documentaciÃ³n de react-native-reanimated
+  ],
 };
